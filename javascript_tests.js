c38e9be6d8d0d90c0403c41314f75a7c898dd6bb
Alexandre Paulino Sierra da Silva <alexandrepaulinoss@users.noreply.github.com>
2022-02-19 09:03:19 -0300

    Create javascript_tests.js

---
+++ javascript_tests.js
@@ -0,0 +1,29 @@
+//Just a "Hello world"
+const message = 'Hello world'
+console.log(message)
+
+//Parameters (tip: use max 3 parameters)
+const test = (var1, var2, var3, var4) => `var1 = ${var1} var2 = ${var2} var3 = ${var3} var4 = ${var4}`
+console.log(test("b", "a", "c", 1))
+
+//Parameters (object)
+const test2 = ({var1, var2, var3, var4}) => `var1 = ${var1} var2 = ${var2} var3 = ${var3} var4 = ${var4}`
+console.log(test2({var2: "b", var1: "a", var3: "c", var4: 1}))
+
+//Replicate an array
+const replicateArray = myArray => myArray.concat(myArray);
+console.log(replicateArray(['1','2']))
+
+//Reverse string
+const reverseString = myString => myString.split("").reverse().join("")
+console.log(reverseString("Hello word"))
+
+//Return the longest word in a sentense
+const longestWord = mySentense => {
+  const sentenseAsArray = mySentense.split(" ")
+  let word = ""
+
+  sentenseAsArray.forEach((arrayItem) => word = arrayItem.length > word.length ? arrayItem: word)
+  return word
+}
+console.log(`The longestWord word is ${longestWord("Hello word! This test should return the first occurrence of the longest word. In this case: occurrenc#")}`)
