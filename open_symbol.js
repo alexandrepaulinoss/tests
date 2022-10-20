d00ff4001d70c7f6160e5d8db3a50b08ae6bfd10
Alexandre Paulino Sierra da Silva <alexandrepaulinoss@users.noreply.github.com>
2022-10-20 16:24:12 -0300

    Create open_symbol.js

---
+++ open_symbol.js
@@ -0,0 +1,37 @@
+// Checks for open (, { or [ on the code
+// It will return true, if an open symbols is found or false if the opened symbols were closed
+
+function isValid(code) {
+
+  const openersToClosers = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+  };
+
+  const openers = new Set(['(', '[', '{']);
+  const closers = new Set([')', ']', '}']);
+
+  const openersStack = [];
+
+  for (let i = 0; i < code.length; i++) {
+    const char = code.charAt(i);
+
+    if (openers.has(char)) {
+      openersStack.push(char);
+    } else if (closers.has(char)) {
+      if (!openersStack.length) {
+        return false;
+      }
+      const lastUnclosedOpener = openersStack.pop();
+
+      // If this closer doesn't correspond to the most recently
+      // seen unclosed opener, short-circuit, returning false
+      if (openersToClosers[lastUnclosedOpener] !== char) {
+        return false;
+      }
+    }
+  }
+  return openersStack.length === 0;
+}
+console.log(isValid('({)'));
